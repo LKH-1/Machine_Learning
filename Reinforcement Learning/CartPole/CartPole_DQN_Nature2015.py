7aa7276ee8c82cb4e7df626028d542f97006525f
jcwleo <jcwleo@naver.com>
2017-02-23 18:22:45 +0900

    Dropout 추가 및 Learning_rate, e-greedy 수정

--- Reinforcement Learning/CartPole/CartPole_DQN_Nature2015.py
+++ Reinforcement Learning/CartPole/CartPole_DQN_Nature2015.py
@@ -18,8 +18,8 @@ INPUT = env.observation_space.shape[0]
 OUTPUT = env.action_space.n
 
 # 하이퍼파라미터
-LEARNING_LATE = 0.1
-NUM_EPISODE = 1000
+LEARNING_LATE = 0.01
+NUM_EPISODE = 10000
 DISCOUNT = 0.99
 
 
@@ -28,32 +28,38 @@ DISCOUNT = 0.99
 x=tf.placeholder(dtype=tf.float32, shape=(None, INPUT))
 
 y=tf.placeholder(dtype=tf.float32, shape=(None, OUTPUT))
+dropout = tf.placeholder(dtype=tf.float32)
 
 # Main 네트워크
 W1 = tf.get_variable('W1',shape=[INPUT, 100],initializer=tf.contrib.layers.xavier_initializer())
-# W2 = tf.get_variable('W2',shape=[100,200],initializer=tf.contrib.layers.xavier_initializer())
+W2 = tf.get_variable('W2',shape=[100,100],initializer=tf.contrib.layers.xavier_initializer())
 # W3 = tf.get_variable('W3',shape=[200,150],initializer=tf.contrib.layers.xavier_initializer())
 W4 = tf.get_variable('W4',shape=[100, OUTPUT],initializer=tf.contrib.layers.xavier_initializer())
 
-L1=tf.nn.tanh(tf.matmul(x,W1))
-# L2=tf.nn.relu(tf.matmul(L1,W2))
+_L1=tf.nn.relu(tf.matmul(x,W1))
+L1=tf.nn.dropout(_L1,dropout)
+_L2=tf.nn.relu(tf.matmul(L1,W2))
+L2=tf.nn.dropout(_L2,dropout)
 # L3=tf.nn.relu(tf.matmul(L2,W3))
-Q_pre = tf.matmul(L1,W4)
+Q_pre = tf.matmul(L2,W4)
 
 
 # Target 네트워크
 W1_r = tf.get_variable('W1_r',shape=[INPUT, 100])
-# W2_r = tf.get_variable('W2_r',shape=[100,200])
+W2_r = tf.get_variable('W2_r',shape=[100,100])
 # W3_r = tf.get_variable('W3_r',shape=[200,150])
 W4_r = tf.get_variable('W4_r',shape=[100, OUTPUT])
 
-L1_r=tf.nn.tanh(tf.matmul(x ,W1_r))
-# L2_r=tf.nn.relu(tf.matmul(L1_r,W2_r))
+L1_r=tf.nn.relu(tf.matmul(x ,W1_r))
+L2_r=tf.nn.relu(tf.matmul(L1_r,W2_r))
 # L3_r=tf.nn.relu(tf.matmul(L2_r,W3_r))
-Q_pre_r = tf.matmul(L1_r,W4_r)
+Q_pre_r = tf.matmul(L2_r,W4_r)
 
 # 총 Reward를 저장해놓을 리스트
-rlist=[]
+rlist=[0]
+recent_rlist=[0]
+
+episode = 0
 
 # Loss function 정의
 cost = tf.reduce_sum(tf.square(y-Q_pre))
@@ -69,19 +75,22 @@ with tf.Session() as sess:
     sess.run(W4_r.assign(W4))
 
     # 에피소드 시작
-    for episode in range(NUM_EPISODE):
+    while np.mean(recent_rlist) < 1000 :
+        episode += 1
 
         # state 초기화
         s = env.reset()
-
+        if len(recent_rlist) > 100:
+            del recent_rlist[0]
         # e-greedy
-        e = 1. / ((episode/15)+1)
+        e = 1. / ((episode/10)+10)
+
         rall = 0
         d = False
         count = 0
 
         # 에피소드가 끝나기 전까지 반복
-        while not d and np.mean(rlist) < 1000:
+        while not d and count < 10000 :
 
             #env.render()
             count += 1
@@ -90,7 +99,7 @@ with tf.Session() as sess:
             s_t = np.reshape(s,[1,INPUT])
 
             # 현재 상태의 Q값을 에측
-            Q = sess.run(Q_pre, feed_dict={x:s_t})
+            Q = sess.run(Q_pre, feed_dict={x:s_t, dropout: 1})
 
             # e-greedy 정책으로 랜덤하게 action 결정
             if e > np.random.rand(1):
@@ -117,7 +126,7 @@ with tf.Session() as sess:
 
 
         # 10번의 episode마다 학습
-        if episode % 10 == 1:
+        if episode % 10 == 1 and len(REPLAY_MEMORY) > 10:
 
             # 50번의 미니배치로 학습
             for i in range(MINIBATCH):
@@ -128,7 +137,7 @@ with tf.Session() as sess:
                     s_t_r, a_r, r_r, s1_r, d_r = sample
 
                     # 꺼내온 리플레이의 state의 Q값을 예측
-                    Q = sess.run(Q_pre, feed_dict={x: s_t_r})
+                    Q = sess.run(Q_pre, feed_dict={x: s_t_r, dropout: 1})
 
 
                     if d_r:
@@ -141,7 +150,7 @@ with tf.Session() as sess:
                         Q[0, a_r] = r_r + DISCOUNT * np.max(Q1)
 
                     # 업데이트 된 Q값으로 main네트워크를 학습
-                    sess.run(train, feed_dict={x: s_t_r, y: Q})
+                    sess.run(train, feed_dict={x: s_t_r, y: Q, dropout:0.7})
 
             # 10번 마다 target 네트워크에 main 네트워크 값을 copy
             sess.run(W1_r.assign(W1))
@@ -149,10 +158,11 @@ with tf.Session() as sess:
 
         # 총 reward의 합을 list에 저장
         rlist.append(rall)
-        print("Episode {} finished after {} timesteps with r={}. Running score: {}".format(episode, count, rall, np.mean(rlist)))
+        print("Episode {} finished after {} timesteps with r={}. Running score: {}".format(episode, count, rall,
+                                                                                           np.mean(rlist)))
 
-
-    env = wrappers.Monitor(env, 'tmp/CartPole_DQN_Nature2015',force='True')
+        if np.mean(rlist) > 400 :
+            break
 
     for episode in range(NUM_EPISODE):
         # state 초기화
@@ -171,7 +181,7 @@ with tf.Session() as sess:
             s_t = np.reshape(s, [1, INPUT])
 
             # 현재 상태의 Q값을 에측
-            Q = sess.run(Q_pre, feed_dict={x: s_t})
+            Q = sess.run(Q_pre, feed_dict={x: s_t,dropout: 1})
             a = np.argmax(Q)
 
             # 결정된 action으로 Environment에 입력
@@ -184,4 +194,4 @@ with tf.Session() as sess:
         rlist.append(rall)
         print("Episode {} finished after {} timesteps with r={}. Running score: {}".format(episode, count, rall,
                                                                                            np.mean(rlist)))
-    gym.upload('tmp/CartPole_DQN_Nature2015', api_key='sk_DM3tWuF4SAeK6H33znC7g')
+
